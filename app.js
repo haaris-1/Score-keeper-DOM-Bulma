03704f80f2a64a7d7ba696f5d7af450bc941ba80
haaris-1 <85678728+haaris-1@users.noreply.github.com>
2022-01-12 11:05:31 +0530

    Added initial index and js file

---
+++ app.js
@@ -0,0 +1,66 @@
+const p1 = document.querySelector('#p1score');
+const p2 = document.querySelector('#p2score');
+const body = document.querySelector('body')
+let p1score = parseInt(`${p1.innerText}`);
+let p2score = parseInt(`${p2.innerText}`);
+
+const p1plus = document.querySelector('#p1b');
+const p2plus = document.querySelector('#p2b');
+
+const playingto = document.querySelector('#playingto');
+const reset = document.querySelector('#reset');
+
+let isGameOver = false;
+let winScore = 5;
+
+const paragraph = document.querySelector('#paragraph');
+
+let newVal = document.createElement('p');
+newVal.innerText = playingto.value;
+paragraph.append(newVal);
+
+p1plus.addEventListener('click',function(){
+
+    if(!isGameOver){
+        p1score++;
+
+        if(p1score === winScore){
+            isGameOver = true;
+            p1.classList.add('winner');
+            p2.classList.add('loser');
+        }
+        p1.textContent = p1score;
+    }
+})
+p2plus.addEventListener('click',function(){
+
+    if(!isGameOver){
+        p2score++;
+
+        if(p2score === winScore){
+            isGameOver = true;
+            p2.classList.add('winner');
+            p1.classList.add('loser');
+        }
+        p2.textContent = p2score;
+    }
+})
+
+playingto.addEventListener('change',function(){
+    
+    winScore = parseInt(this.value);
+    resetfun();
+})
+
+reset.addEventListener('click' , resetfun)
+
+
+function resetfun(){
+    isGameOver = false;
+    p1score = 0;
+    p2score = 0;
+    p1.textContent = 0;
+    p2.textContent = 0;
+    p1.classList.remove('winner','loser'); 
+    p2.classList.remove('winner','loser'); 
+}
